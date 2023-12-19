1e6f1f350d335c355ede5a97aa1b0a6d8f947b67
heyEdem <akatoaugustine@gmail.com>
2023-12-19 18:04:35 +0000

    first commit

---
+++ src/main/java/com/edem/bookmarker/BookmarkerApplication.java
@@ -0,0 +1,13 @@
+package com.edem.bookmarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkerApplication.class, args);
+	}
+
+}
