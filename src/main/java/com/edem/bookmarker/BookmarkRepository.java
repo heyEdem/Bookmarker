9fa8e3d1513b0a5f1d4bf88c64ca96734a30f25b
heyEdem <akatoaugustine@gmail.com>
2023-12-19 20:25:49 +0000

    Bookmark Repository

---
+++ src/main/java/com/edem/bookmarker/BookmarkRepository.java
@@ -0,0 +1,8 @@
+package com.edem.bookmarker;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+}
