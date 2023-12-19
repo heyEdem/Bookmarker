5b41fbb0faa347bb1a837ce6dc98bb98c5cc12c1
heyEdem <akatoaugustine@gmail.com>
2023-12-19 20:24:00 +0000

    Bookmark entity

---
+++ src/main/java/com/edem/bookmarker/Bookmarker.java
@@ -0,0 +1,32 @@
+package com.edem.bookmarker;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "bookmarks")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bookmarker {
+    @Id
+    @SequenceGenerator(name = " ", sequenceName = "bm_id_sec")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bm_id_sec_gen")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String url;
+
+
+
+    private Instant createdAt;
+}
