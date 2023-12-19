7fd1c15f9ce03eb58e95d14ce373c90d539c2ab4
heyEdem <akatoaugustine@gmail.com>
2023-12-19 21:07:33 +0000

    Bookmark entity fixup!

--- src/main/java/com/edem/bookmarker/Bookmark.java
+++ src/main/java/com/edem/bookmarker/Bookmark.java
@@ -14,7 +14,7 @@ import java.time.Instant;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Bookmarker {
+public class Bookmark {
     @Id
     @SequenceGenerator(name = " ", sequenceName = "bm_id_sec")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bm_id_sec_gen")
