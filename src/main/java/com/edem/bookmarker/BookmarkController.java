aacdc1d70dabd7ca418c631639276dca84a43511
heyEdem <akatoaugustine@gmail.com>
2023-12-20 23:33:32 +0000

    BookmarkController (Pageable)

--- src/main/java/com/edem/bookmarker/BookmarkController.java
+++ src/main/java/com/edem/bookmarker/BookmarkController.java
@@ -3,6 +3,7 @@ package com.edem.bookmarker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping
-    public List<Bookmark> getBookmarks(){
-        return bookmarkService.getBookmarks();
+    public List<Bookmark> getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page){
+        return bookmarkService.getBookmarks(page);
     }
 }
