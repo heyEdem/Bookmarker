34c332bd471344fe5195d02939ee95686253b00b
heyEdem <akatoaugustine@gmail.com>
2023-12-23 23:46:18 +0000

    insert sample data sql for h2 and postgresql

---
+++ src/main/resources/db/migration/h2/V2__insert_sample_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO bookmarks (title, url, created_at) VALUES
+('twitter','https://twitter.com', CURRENT_TIMESTAMP()),
+('snapchat','https://snapchat.com', CURRENT_TIMESTAMP()),
+('google','https://google.com', CURRENT_TIMESTAMP()),
+('facebook','https://facebook.com', CURRENT_TIMESTAMP()),
+('telegram','https://telegram.com', CURRENT_TIMESTAMP()),
+('gmail','https://gmail.com', CURRENT_TIMESTAMP()),
+('instagram','https://instagram.com', CURRENT_TIMESTAMP()),
+('pinterest','https://pinterest.com', CURRENT_TIMESTAMP()),
+('fabian','https://fabian.com', CURRENT_TIMESTAMP()),
+('steven','https://steven.com', CURRENT_TIMESTAMP()),
+('slightly','https://slightly.com', CURRENT_TIMESTAMP()),
+('dan','https://dan.com', CURRENT_TIMESTAMP()),
+('harvey','https://harvey.com', CURRENT_TIMESTAMP()),
+('mike','https://nike.com', CURRENT_TIMESTAMP()),
+('randell','https://randell.com', CURRENT_TIMESTAMP());
