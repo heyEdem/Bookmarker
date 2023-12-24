901a1148e2038c50c0e9f89184fe301a50c23189
heyEdem <akatoaugustine@gmail.com>
2023-12-24 23:25:51 +0000

    Bookmark Data access mapper

--- src/main/java/com/edem/bookmarker/BookmarkDTO.java
+++ src/main/java/com/edem/bookmarker/BookmarkDTO.java
@@ -1,34 +1,23 @@
 package com.edem.bookmarker;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.domain.Page;
 
-import java.util.List;
+import java.time.Instant;
 
-@Setter
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookmarkDTO {
-    private List<Bookmark> data;
-    private long totalElements;
-    private int totalPages;
-    private int currentPage;
-    @JsonProperty("isFirst")
-    private boolean isFirst;
-    @JsonProperty("isLast ")
-    private boolean isLast;
-    private boolean hasPrevious;
-    private boolean hasNext;
+    private Long id;
 
-    public BookmarkDTO(Page<Bookmark> bookmarkPage){
-        this.setData(bookmarkPage.getContent());
-        this.setTotalElements(bookmarkPage.getTotalElements());
-        this.setTotalPages(bookmarkPage.getTotalPages());
-        this.setCurrentPage(bookmarkPage.getNumber() + 1);
-        this.setFirst(bookmarkPage.isFirst());
-        this.setLast(bookmarkPage.isLast());
-        this.setHasNext(bookmarkPage.hasNext());
-        this.setHasPrevious(bookmarkPage.hasPrevious());
-    }
+    private String title;
+
+    private String url;
+
+    private Instant createdAt;
 }
