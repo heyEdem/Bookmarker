1fdacee39a18801af6cc919a0a51786a7131bc92
heyEdem <akatoaugustine@gmail.com>
2023-12-25 23:30:09 +0000

    Bookmark Data access mapper

---
+++ src/main/java/com/edem/bookmarker/BookmarkMapper.java
@@ -0,0 +1,15 @@
+package com.edem.bookmarker;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookmarkMapper {
+    public BookmarkDTO toDTO(Bookmark bookmark){
+        BookmarkDTO bookmarkDTO = new BookmarkDTO();
+        bookmarkDTO.setId(bookmark.getId());
+        bookmarkDTO.setUrl(bookmark.getUrl());
+        bookmarkDTO.setTitle(bookmark.getTitle());
+        bookmarkDTO.setCreatedAt(bookmark.getCreatedAt());
+        return bookmarkDTO;
+    }
+}
