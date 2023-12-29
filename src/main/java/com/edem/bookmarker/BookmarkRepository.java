adbfecb41744a8f582e49c159ce0f4fd1858b848
heyEdem <akatoaugustine@gmail.com>
2023-12-29 23:59:20 +0000

    Search bookmark

--- src/main/java/com/edem/bookmarker/BookmarkRepository.java
+++ src/main/java/com/edem/bookmarker/BookmarkRepository.java
@@ -10,4 +10,13 @@ import org.springframework.stereotype.Repository;
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     @Query("select new com.edem.bookmarker.BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b")
     Page<BookmarkDTO> findBookmarks(Pageable pageable);
+
+    @Query("""
+            select new com.edem.bookmarker.BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b 
+            where lower(b.title) like lower(concat('%', :query, '%'))
+            """)
+
+    Page<BookmarkDTO> searchBookmarks(String query, Pageable pageable);
+
+    Page<BookmarkDTO> findByTitleContainsIgnoreCase(String query , Pageable pageable);
 }
