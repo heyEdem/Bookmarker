30599d758feb7a99ca88af0d2d2796e57b00fe68
heyEdem <akatoaugustine@gmail.com>
2023-12-30 23:59:24 +0000

    search / create new bookmark

--- src/main/java/com/edem/bookmarker/BookmarkService.java
+++ src/main/java/com/edem/bookmarker/BookmarkService.java
@@ -8,17 +8,35 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class BookmarkService {
 
     private final BookmarkRepository bookmarkRepository;
+    private final BookmarkMapper mapper;
 
     public BookmarksDTO getBookmarks(Integer page){
         int pageNo = page < 1 ? 0 : page -1;
         Pageable pageable = PageRequest.of(pageNo,10, Sort.Direction.DESC, "createdAt");
         Page<BookmarkDTO> bookmarkPage =  bookmarkRepository.findBookmarks(pageable);
         return new BookmarksDTO(bookmarkPage);
+    }
+
+    @Transactional
+    public BookmarksDTO searchBookmarks(String query,Integer page ) {
+        int pageNo = page < 1 ? 0 : page -1;
+        Pageable pageable = PageRequest.of(pageNo,10, Sort.Direction.DESC,"createdAt");
+//        Page<BookmarkDTO> bookmarkPage = bookmarkRepository.searchBookmarks(query, pageable);
+        Page<BookmarkDTO> bookmarkPage = bookmarkRepository.findByTitleContainsIgnoreCase(query, pageable);
+        return new BookmarksDTO(bookmarkPage);
+    }
+
+    public BookmarkDTO createBookmark(CreateBookmarkRequest request) {
+        Bookmark bookmark = new Bookmark( null, request.getTitle(),request.getUrl(), Instant.now());
+        Bookmark savedBookmark = bookmarkRepository.save(bookmark);
+        return mapper.toDTO(savedBookmark);
     }
 }
