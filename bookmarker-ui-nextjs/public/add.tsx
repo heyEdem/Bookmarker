af52e703ced8dcc0fb1e0871d1eeadaf13a5d16d
heyEdem <akatoaugustine@gmail.com>
2024-02-11 23:09:27 +0000

    Add bookmark page

---
+++ bookmarker-ui-nextjs/public/add.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import React from "react";
+
+
+const AddBookmark: NextPage = () => {
+    return(
+        <div>
+          <h1>New Bookmark Form</h1>
+        </div>
+    )
+}
+
+export default AddBookmark
